340a90248888f442847fe586bca30361c3c9ccc8
mooss <mooss@protonmail.com>
2025-03-25 21:07:10 +0100

    Render 3d grid with colors interpolated on height

--- p5js/grid-3d.js
+++ p5js/grid-3d.js
@@ -1,6 +1,7 @@
 let gridSize = 20; // Number of cells in each direction
 let cellSize = 30; // Size of each cell
 let grid = [];
+let maxH = 100;
 
 function setup() {
     createCanvas(800, 600, WEBGL);
@@ -24,8 +25,12 @@ function draw() {
     for (let i = 0; i < gridSize; i++) {
         for (let j = 0; j < gridSize; j++) {
             push();
-            translate(i * cellSize, j * cellSize, grid[i][j] / 2); // Position the cell.
-            box(cellSize, cellSize, grid[i][j]); // Draw the cell as a box.
+            // Interpolate color between magenta and cyan using the height.
+            let height = grid[i][j];
+            fill(interpolate([[255, 0, 255], [0, 255, 255]], height/maxH));
+
+            translate(i * cellSize, j * cellSize, height / 2); // Position the cell.
+            box(cellSize, cellSize, height); // Draw the cell as a box.
             pop();
         }
     }
@@ -42,7 +47,63 @@ function randGrid() {
     for (let i = 0; i < gridSize; i++) {
         grid[i] = [];
         for (let j = 0; j < gridSize; j++) {
-            grid[i][j] = random(20, 100); // Random height between 20 and 100
+            grid[i][j] = random(0, maxH);
         }
     }
+}
+
+//:CONCAT lib.js
+// Everything after this point was generated with `./refresh.bash grid-3d.js`.
+
+//lib.js
+// Color spectrum: white -> rainbow -> black.
+const wrainbowb = [
+    [255, 255, 255], // white
+    [255, 0, 0],     // red
+    [255, 255, 0],   // yellow
+    [0, 255, 0],     // green
+    [0, 255, 255],   // cyan
+    [0, 0, 255],     // blue
+    [255, 0, 255],   // magenta
+    [0, 0, 0],       // black
+];
+
+// Return x bounded by min_ and max_.
+function clamp(x, min_, max_) {
+    if (x < min_) return min_;
+    if (x > max_) return max_;
+    return x;
+}
+
+// Interpolate the value (expected to be between 0 and 1) in the given color spectrum.
+function interpolate(colors, value) {
+    if (colors.length == 0) {
+        return [255, 255, 255];
+    }
+    if (colors.length == 1) {
+        return colors[0];
+    }
+
+	value = clamp(value, 0, 1);
+
+    // Find the position on the spectrum.
+    const nsegments = colors.length-1;
+    const segment = clamp(Math.floor(value * nsegments, 0, 5));
+    const ratio = value * nsegments - segment;
+
+    // Get the two colors to interpolate between.
+    const color1 = colors[segment];
+    const color2 = colors[segment + 1];
+
+    // Interpolate between the two colors.
+    const r = Math.floor(color1[0] + (color2[0] - color1[0]) * ratio);
+    const g = Math.floor(color1[1] + (color2[1] - color1[1]) * ratio);
+    const b = Math.floor(color1[2] + (color2[2] - color1[2]) * ratio);
+
+	return color(r, g, b);
+}
+
+// Return a random color.
+function colorand() {
+    return color(random(255), random(255), random(255));
 }
