2b85d521d738920b9f843dc9a45b16c498624d93
mooss <mooss@protonmail.com>
2025-04-06 00:22:21 +0200

    Use off-white instead of white for mountain tops

--- web/palettes.js
+++ web/palettes.js
@@ -1,13 +1,13 @@
 function rgb(r, g, b) { return new THREE.Color(r/255, g/255, b/255); };
 
 export const terrainPalette = [
-    rgb(100, 200, 50),  // Light green.
-    rgb(100, 200, 50),  // Light green.
-    rgb(75, 125, 25),   // Dark green.
-    rgb(75, 125, 25),   // Dark green.
-    rgb(100, 100, 100), // Grey.
-    rgb(255, 255, 255), // White.
-    rgb(255, 255, 255), // White.
+    rgb(100, 200, 50),  // Plains.
+    rgb(100, 200, 50),  // Plains.
+    rgb(75, 125, 25),   // Forests.
+    rgb(75, 125, 25),   // Forests.
+    rgb(100, 100, 100), // Mountains.
+    rgb(220, 220, 220), // Mountain tops.
+    rgb(220, 220, 220), // Mountain tops.
 ];
 
 // Interesting for the underside which can be used to seed continents.
