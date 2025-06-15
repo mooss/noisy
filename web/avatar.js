4020bbcd0444650ab4912ec0dd7efbe4dd13cf0f
mooss <mooss@protonmail.com>
2025-06-15 10:49:43 +0200

    Simplify Avatar class

--- web/avatar.js
+++ web/avatar.js
@@ -2,31 +2,20 @@
  * The player avatar.
  */
 export class Avatar {
-    /**
-     * @private
-     * @type {THREE.Mesh}
-     */
-    #mesh;
+    /** @type {THREE.Mesh} */
+    mesh;
 
     constructor() {
         const geometry = new THREE.SphereGeometry(1, 32, 32);
         const material = new THREE.MeshStandardMaterial({ color: 0xff0000 }); // Red sphere.
-        this.#mesh = new THREE.Mesh(geometry, material);
-    }
-
-    /**
-     * Gets the underlying Three.js Mesh object of the avatar.
-     * @returns {THREE.Mesh} The Three.js Mesh object.
-     */
-    get mesh() {
-        return this.#mesh;
+        this.mesh = new THREE.Mesh(geometry, material);
     }
 
     setPosition(x, y, z) {
-        this.#mesh.position.set(x, y, z);
+        this.mesh.position.set(x, y, z);
     }
 
     setScale(scale) {
-        this.#mesh.scale.set(scale, scale, scale);
+        this.mesh.scale.set(scale, scale, scale);
     }
 }
