6cac22a4634fc643cf071506d0513b9828219646
mooss <mooss@protonmail.com>
2025-07-28 22:47:31 +0200

    Rename version number to version code
    Putting alpha in the version code removes all possible confusion that
    this could be a release version.

--- web/constants.ts
+++ web/constants.ts
@@ -1,7 +1,7 @@
 // Base dimension of a chunk in 3d space in world units.
 export const CHUNK_UNIT: number = 256;
 
-export const VERSION_NUMBER: string = '1';
+export const VERSION_CODE: string = 'alpha 1';
 export const VERSION_NAME: string = 'acorn';
-export interface Version { name: string; number: string; }
-export const VERSION: Version = { name: VERSION_NAME, number: VERSION_NUMBER };
+export interface Version { name: string; code: string; }
+export const VERSION: Version = { name: VERSION_NAME, code: VERSION_CODE };
