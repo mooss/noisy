65e6498e3f52d1eab69bf413845938f828cc883f
mooss <mooss@protonmail.com>
2025-09-02 15:27:38 +0200

    Separate version period from version number in versioning system

--- web/constants.ts
+++ web/constants.ts
@@ -1,7 +1,8 @@
 // Base dimension of a chunk in 3d space in world units.
 export const CHUNK_UNIT: number = 256;
 
-export const VERSION_CODE: string = 'alpha 1';
+export const VERSION_PERIOD: string = 'alpha';
+export const VERSION_NUMBER: string = '1';
 export const VERSION_NAME: string = 'acorn';
-export interface Version { name: string; code: string; }
-export const VERSION: Version = { name: VERSION_NAME, code: VERSION_CODE };
+export interface Version { period: string, number: string, name: string; }
+export const VERSION: Version = { period: VERSION_PERIOD, number: VERSION_NUMBER, name: VERSION_NAME };
