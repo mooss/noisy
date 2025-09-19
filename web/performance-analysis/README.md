9cbf666cd911aa7ed5a9dd67785c540a4664250e
mooss <mooss@protonmail.com>
2025-09-19 16:54:55 +0200

    Measure initial performance of surface mesh creation

--- web/performance-analysis/README.md
+++ web/performance-analysis/README.md
@@ -1,3 +1,20 @@
 # Profiling and benchmarks
 
 This directory contains scripts to analyse the performance of the project and to identify and benchmark bottlenecks.
+
+## Benchmarking `createSurfaceMesh`
+
+This section summarizes the optimization process of `createSurfaceMesh` that a quick profiling revealed to be a major bottleneck.
+The benchmark are executed several times to be sure that the results are actually consistent, because they can vary wildly on some machines (probably due to thermal throttling).
+
+The benchmarks are executed on a Rizen 9 7950X and measure the throughput of mesh creation.
+
+### Initial performances
+
+| Run | Mean         | Median   | Samples |
+|-----|--------------|----------|---------|
+|   1 |  125 ± 0.83% |  129 ± 2 |     250 |
+|   2 |  125 ± 0.95% |  129 ± 1 |     248 |
+|   3 |  125 ± 0.85% |  129 ± 1 |     249 |
+|   4 |  125 ± 0.85% |  128 ± 1 |     249 |
+|   5 |  124 ± 0.85% |  128 ± 1 |     248 |
--- web/performance-analysis/mesh.ts
+++ web/performance-analysis/mesh.ts
@@ -1,12 +1,36 @@
 import { Bench, TaskResult } from 'tinybench';
-import { INITIAL_STATE } from "../init.js";
+import { Layered, Simplex } from '../noise/algorithms.js';
+import { RenderState } from '../state/render.js';
 
-const state = INITIAL_STATE;
 (globalThis as any).sink = 0;
 
+// The noise function responsible for terrain creation.
+const noise = new Layered({
+    noise: new Simplex({ seed: 23 }),
+    layers: {
+        fundamental: .7,
+        octaves: 8,
+        persistence: .65,
+        lacunarity: 1.5,
+    },
+    sampling: { size: 30, threshold: 2.5, fundamental: 3 },
+});
+
+// Dictates the mesh function that will be used.
+const render = new RenderState({
+    style: 'surface', // Will use createSurfaceMesh.
+    // The rest is not relevant and only here to placate the type system.
+    paletteName: 'Bright terrain',
+    light: {
+        ambient: { intensity: .5 },
+        directional: { intensity: 4 },
+    },
+    heightMultiplier: 1,
+});
+
 const mkmesh = (side: number) => {
-    (globalThis as any).sink = state.render.mesh({
-        at: state.noise.normalised(.01, 1),
+    (globalThis as any).sink = render.mesh({
+        at: noise.normalised(.01, 1),
         nblocks: side,
     });
 };
@@ -23,12 +47,12 @@ function throughput(res: TaskResult) {
     };
 }
 
-console.log('')
+console.log('Throughput (iterations per second) of `createSurfaceMesh`.')
 console.log('| Run | Mean         | Median   | Samples |');
 console.log('|-----|--------------|----------|---------|');
 
 const b = new Bench({
-    time: 1000,
+    time: 2000,
     warmupIterations: 128,
 });
 b.add('mesh128', () => mkmesh(128));
