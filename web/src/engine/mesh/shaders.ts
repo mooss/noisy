63480873f6dc51d4b2294d5dda6c687011dc7de3
mooss <mooss@protonmail.com>
2025-10-18 05:52:29 +0200

    Cleanup shader injection

--- web/src/engine/mesh/shaders.ts
+++ web/src/engine/mesh/shaders.ts
@@ -1,6 +1,40 @@
 import * as THREE from 'three';
 import { Palette } from "../palettes.js";
 
+interface ShaderInjection { decl?: string; impl?: string; }
+export function injectInShader(
+    material: THREE.Material,
+    vertex: ShaderInjection,
+    fragment: ShaderInjection,
+    uniforms: Record<string, any>,
+): THREE.Material {
+    material.onBeforeCompile = shader => {
+        for (const [name, value] of Object.entries(uniforms))
+            shader.uniforms[name] = { value };
+
+        shader.vertexShader = inject(
+            vertex.decl || '', vertex.impl || '',
+            shader.vertexShader, `#include <begin_vertex>`,
+        );
+
+        shader.fragmentShader = inject(
+            fragment.decl || '', fragment.impl || '',
+            shader.fragmentShader, '#include <color_fragment>',
+        );
+    };
+    return material;
+}
+
+const paletteDecl = `
+uniform sampler2D u_palette;
+uniform float u_paletteWidth;
+varying float v_z;
+`;
+const paletteDiffuse = `
+vec2 colorIdx = vec2(mix(0.5 / u_paletteWidth, 1.0 - 0.5 / u_paletteWidth, v_z), 0.5);
+diffuseColor.rgb = texture2D(u_palette, colorIdx).rgb;
+`
+
 /**
  * Create a mesh material that interpolates colors from height in the shaders by storing the color
  * palette in a texture.
@@ -8,49 +42,35 @@ import { Palette } from "../palettes.js";
  * @param palette - The color palette used for shading.
  * @returns a material configured with custom shader logic for palette-based coloring.
  */
-export function paletteShader(palette: Palette): THREE.MeshStandardMaterial {
+export function paletteShader(palette: Palette): THREE.Material {
     //TODO: Handle texture disposal.
-    // The color palette is stored as a texture.
-    const paletteTex = new THREE.DataTexture(
+    const paletteTex = palette2texture(palette);
+
+    // The shader created by THREE.js contains a lot of useful things so it is simpler for now to
+    // inject additional instructions rather than to write a shader from scratch.
+    return injectInShader(
+        new THREE.MeshStandardMaterial(),
+        { decl: 'varying float v_z;', impl: 'v_z = position.z;' },
+        { decl: paletteDecl, impl: paletteDiffuse },
+        { u_palette: paletteTex, u_paletteWidth: palette.size },
+    )
+}
+
+function palette2texture(palette: Palette): THREE.DataTexture {
+    const res = new THREE.DataTexture(
         palette.toArray(),
         palette.size, // Width.
         1,            // Height.
         THREE.RGBAFormat,
         THREE.FloatType,
     );
-    paletteTex.needsUpdate = true;
-    paletteTex.magFilter = THREE.LinearFilter;
-    paletteTex.minFilter = THREE.LinearMipMapLinearFilter;
-    paletteTex.wrapS = THREE.ClampToEdgeWrapping;
+    res.needsUpdate = true;
+    res.magFilter = THREE.LinearFilter;
+    res.minFilter = THREE.LinearMipMapLinearFilter;
+    res.wrapS = THREE.ClampToEdgeWrapping;
+    return res;
+}
 
-    // The shader created by THREE.js contains a lot of useful things so it is simpler for now to
-    // inject additional instructions rather than to write a shader from scratch.
-    const material = new THREE.MeshStandardMaterial();
-    material.onBeforeCompile = shader => {
-        // Vertex shader: forward the vertex height to the fragment shader.
-        const vertexWithMeshPosition = shader.vertexShader.replace(
-            `#include <begin_vertex>`,
-            `#include <begin_vertex>
-v_z = position.z;`
-        );
-        shader.vertexShader = `varying float v_z;
-${vertexWithMeshPosition}`;
-
-        // Fragment shader: smoothly interpolate the color from the height using the palette.
-        // Without this, the transition between two colors is too fast, which is particularly
-        // visible with a bicolor palette.
-        const injectColor = `#include <color_fragment>
-vec2 colorIdx = vec2(mix(0.5 / u_paletteWidth, 1.0 - 0.5 / u_paletteWidth, v_z), 0.5);
-diffuseColor.rgb = texture2D(u_palette, colorIdx).rgb;`
-        const fragmentWithColor = shader.fragmentShader.replace('#include <color_fragment>', injectColor);
-        shader.uniforms.u_palette = { value: paletteTex };
-        shader.uniforms.u_paletteWidth = { value: palette.size };
-        shader.fragmentShader = `uniform sampler2D u_palette;
-
-uniform float u_paletteWidth;
-varying float v_z;
-${fragmentWithColor}`;
-    };
-
-    return material;
+function inject(prelude: string, content: string, shader: string, marker: string): string {
+    return prelude + '\n' + shader.replace(marker, marker + '\n' + content);
 }
