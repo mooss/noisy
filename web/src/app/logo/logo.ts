113935152c02b8ce4e09a8790a36592dd010751c
mooss <mooss@protonmail.com>
2025-11-21 18:17:10 +0100

    Specify the base rhombus by flipping the base triangle

--- web/src/app/logo/logo.ts
+++ web/src/app/logo/logo.ts
@@ -1,12 +1,32 @@
 import { vector2 } from "../../maths/maths.js";
 
-const NOGAP = 1; // Small number to avoid gaps between shapes.
-const OUTLINE_PROPORTION = .03; // Configurable outline thickness
+
+// Amount of radians corresponding to 1/3 of a rotation.
+const RAD_THIRD = 2 * Math.PI / 3;
+
+// Amount of radians corresponding to 1/6 of a rotation.
+const RAD_SIXTH = Math.PI / 3;
+
+// Amount of radians corresponding to 1/12 of a rotation.
+const RAD_TWELFTH = Math.PI / 6;
+
+// Small number to avoid gaps between shapes.
+const NOGAP = 1;
+
+// Thickness of the outer lines.
+const OUTLINE_PROPORTION = .03;
+
+// Thickness of the inner lines.
+const INLINE_PROPORTION = .015;
+
+// Dimensions of the beam.
+const BEAM_PROPORTION = .2;
+
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d')!;
 
 function resize() {
-    const size = Math.min(window.innerWidth, window.innerHeight) * 0.8;
+    const size = Math.min(window.innerWidth, window.innerHeight);
     canvas.width = size;
     canvas.height = size;
 }
@@ -34,31 +54,36 @@ function poly(...points: vector2[]) {
     ctx.fill();
 }
 
-
-// Amount of radians corresponding to 1/3 of a rotation.
-const RAD_THIRD = 2 * Math.PI / 3;
-// Amount of radians corresponding to 1/12 of a rotation.
-const RAD_TWELFTH = Math.PI / 6;
-
 /**
- * Draw a third of the logo.
+ * Draw a sixth of the logo (the bottom left triangle).
  */
-function third(size: number, color: string) {
+function sixth(size: number, color: string) {
     const median = size * Math.cos(RAD_TWELFTH);
     const top = { x: 0, y: 0 };
-    const right = { x: median, y: size/2 };
     const bottom = { x: 0, y: size };
-    const left = { x: -median, y: size/2 };
+    const left = { x: -median, y: size / 2 };
 
-    // Solid base.
+    // Solid base (triangle).
     ctx.fillStyle = color;
-    poly(top, right, bottom, left);
+    poly(top, bottom, left);
 
     // Outline.
     ctx.strokeStyle = 'black';
     ctx.lineCap = 'round';
     ctx.lineWidth = size * OUTLINE_PROPORTION;
-    line(right, bottom, left);
+    line(bottom, left);
+}
+
+/**
+ * Draw a third of the logo (the bottom rhombus, made of the bottom left triangle and its
+ * horizontally mirrored sibling).
+ */
+function third(size: number, color: string) {
+    ctx.save();
+    sixth(size, color);
+    ctx.scale(-1, 1);
+    sixth(size, color);
+    ctx.restore();
 }
 
 function logo() {
