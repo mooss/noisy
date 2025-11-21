6acb3b840ac6eb3b9d8f99ea7ea76a26698a79be
mooss <mooss@protonmail.com>
2025-11-21 18:56:56 +0100

    Construct lines around the inner fills

--- web/src/app/logo/logo.ts
+++ web/src/app/logo/logo.ts
@@ -1,6 +1,5 @@
 import { vector2 } from "../../maths/maths.js";
 
-
 // Amount of radians corresponding to 1/3 of a rotation.
 const RAD_THIRD = 2 * Math.PI / 3;
 
@@ -14,13 +13,13 @@ const RAD_TWELFTH = Math.PI / 6;
 const NOGAP = 1;
 
 // Thickness of the outer lines.
-const OUTLINE_PROPORTION = .03;
+const OUTLINE_PROPORTION = .02;
 
 // Thickness of the inner lines.
-const INLINE_PROPORTION = .015;
+const INLINE_PROPORTION = .02;
 
 // Dimensions of the beam.
-const BEAM_PROPORTION = .2;
+const BEAM_PROPORTION = .1;
 
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d')!;
@@ -58,20 +57,56 @@ function poly(...points: vector2[]) {
  * Draw a sixth of the logo (the bottom left triangle).
  */
 function sixth(size: number, color: string) {
+    ///////////////////
+    // Triangle base //
     const median = size * Math.cos(RAD_TWELFTH);
     const top = { x: 0, y: 0 };
     const bottom = { x: 0, y: size };
     const left = { x: -median, y: size / 2 };
 
-    // Solid base (triangle).
     ctx.fillStyle = color;
     poly(top, bottom, left);
 
-    // Outline.
+    ////////////////////
+    // External lines //
     ctx.strokeStyle = 'black';
     ctx.lineCap = 'round';
     ctx.lineWidth = size * OUTLINE_PROPORTION;
     line(bottom, left);
+
+    ///////////////////////
+    // Inner line points //
+    // Vertical/horizontal size of the beams.
+    const beamsize = size * BEAM_PROPORTION;
+
+    // Median of a beam (i.e. length of the median of the triangle whole side is the beam size).
+    // Used to construct a beam of the desired length when taking the "obliqueness" of the beams
+    // into account.
+    const beamed = beamsize * Math.cos(RAD_TWELFTH);
+
+    // Middle points.
+    const tole = { x: left.x / 2, y: left.y / 2 }; // Middle of top and left points.
+    const mid = { x: 0, y: size / 2 }; // Middle of the rhombus base.
+
+    // Bottom fill points.
+    const bfa = { x: left.x + beamed, y: left.y + beamsize / 2 };
+    const bfb = { x: tole.x, y: tole.y + beamsize }
+    const bfc = { x: mid.x, y: mid.y + beamsize };
+
+    // Small inner fill points.
+    const ifa = { x: tole.x + beamed, y: tole.y - beamsize / 2 };
+    const ifb = { x: top.x - beamed, y: top.y + beamsize / 2 };
+    const ifc = { x: mid.x - beamed, y: mid.y - 1.5 * beamsize };
+
+    ////////////////////
+    // Internal lines //
+    ctx.lineWidth = size * INLINE_PROPORTION;
+
+    // Bottom fill.
+    line(bfa, bfb, bfc);
+
+    // Small inner fill.
+    line(ifa, ifc, ifb);
 }
 
 /**
