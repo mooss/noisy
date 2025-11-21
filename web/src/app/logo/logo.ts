06b9b0473053a407979a818d63e290b62599e11e
mooss <mooss@protonmail.com>
2025-11-21 15:34:43 +0100

    Add outline to logo

--- web/src/app/logo/logo.ts
+++ web/src/app/logo/logo.ts
@@ -1,6 +1,7 @@
 import { vector2 } from "../../maths/maths.js";
 
 const NOGAP = 1; // Small number to avoid gaps between shapes.
+const OUTLINE_PROPORTION = .3; // Configurable outline thickness
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d')!;
 
@@ -15,36 +16,59 @@ function refresh() {
     logo();
 }
 
-function triangle(size: number, color: string) {
+function line(from: vector2, to: vector2) {
     ctx.beginPath();
-    ctx.moveTo(0, -size);
-    ctx.lineTo(size * Math.cos(Math.PI / 6), size * Math.sin(Math.PI / 6));
-    ctx.lineTo(-size * Math.cos(Math.PI / 6), size * Math.sin(Math.PI / 6));
+    ctx.moveTo(from.x, from.y);
+    ctx.lineTo(to.x, to.y);
+    ctx.stroke();
+}
+
+function triangle(size: number, color: string) {
+    const top = { x: 0, y: -size };
+    const right = { x: size * Math.cos(Math.PI / 6), y: size * Math.sin(Math.PI / 6) };
+    const left = { x: -right.x, y: right.y };
+    ctx.save();
+
+    // Inside.
+    ctx.beginPath();
+    ctx.moveTo(top.x, top.y);
+    ctx.lineTo(right.x, right.y);
+    ctx.stroke();
+    ctx.lineTo(left.x, left.y);
     ctx.closePath();
     ctx.fillStyle = color;
     ctx.fill();
+
+    // Border.
+    ctx.strokeStyle = 'black';
+    ctx.lineCap = 'round';
+    ctx.lineWidth = size * OUTLINE_PROPORTION;
+
+    line(left, top);
+    line(top, right);
 }
+
 
 /**
  * Draw a third of the logo.
  */
 function third(size: number, color: string, center: vector2) {
+    ctx.save();
     ctx.translate(center.x, center.y);
+    ctx.rotate(Math.PI / 2);
     triangle(size, color);
     ctx.translate(0, size - NOGAP);
-    ctx.rotate(60 * Math.PI / 180);
+    ctx.rotate(Math.PI);
     triangle(size, color);
+    ctx.restore();
 }
 
 function logo() {
     const centerX = canvas.width / 2;
     const centerY = canvas.height / 2;
-    const triangleSize = Math.min(canvas.width, canvas.height) * 0.25;
-
+    const triangleSize = Math.min(canvas.width, canvas.height) * 0.22;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.save();
     third(triangleSize, 'teal', { x: centerX, y: centerY });
-    ctx.restore();
 }
 
 window.addEventListener('resize', refresh);
